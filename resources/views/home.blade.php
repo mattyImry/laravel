9a5b418b91f77dac81bc6c708c3f6e191422b3af
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-18 16:05:52 +0100

    setting home page layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5"> 
+            <img src="https://cdn.dribbble.com/users/3547568/screenshots/14395014/media/0b94c75b97182946d495f34c16eab987.jpg?compress=1&resize=400x300&vertical=top" alt="" class="rounded-circle" style="height: 200px"> 
+        </div>
+        <div class="col-9 pt-5">
+            <div ><h1>SoundIn</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> posts  </div>
+                <div class="pe-5"><strong>23K</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
             </div>
         </div>
+
     </div>
 </div>
-@endsection
+@endsection class=""
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    <div><img src="/svg/scatterbrain.svg" style="height: 50px" alt=""></div>
-                    <div>SoundIn</div> 
+                    <div><img src="/svg/scatterbrain.svg" style="height: 50px"></div>
+                    <div class="pt-3 ps-1 ">SoundIn</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
