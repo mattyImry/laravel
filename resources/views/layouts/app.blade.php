1b1dac739b1f5646f2f133bf21f7eb83268a46e9
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-18 14:48:43 +0100

    add svg logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Imry
+                    <div><img src="/svg/scatterbrain.svg" alt=""></div>
+                    <div>Imry</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
