6bee812b5c58fd0e0ff99ca4e414c40191d52945
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-18 16:30:56 +0100

    setting up pics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,23 @@
                 <div class="pe-5"><strong>23K</strong> followers</div>
                 <div class="pe-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4"><strong>soundIn.mus</strong></div>
+            <div> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            <div><a href="#">www.soundIn.mus</a></div>
         </div>
 
     </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img class="w-50"  src="https://scontent-fra3-1.cdninstagram.com/v/t51.2885-15/117255331_294550334984177_3775971530383009149_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent-fra3-1.cdninstagram.com&_nc_cat=105&_nc_ohc=0_QUywbjnswAX-sT8cg&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MjM2ODg5NDI1ODUyMTY0NDkzNw%3D%3D.2-ccb7-5&oh=00_AfDWCwlhTRS-gU9BjZh1USDcXSDVTEceGMHQmWkqZLrsVw&oe=644434FB&_nc_sid=1527a3" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-50" style="height: " src="https://scontent-frt3-2.cdninstagram.com/v/t51.2885-15/93810933_272001853828184_5085917630409923189_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_cat=108&_nc_ohc=2Uje8GAJjD0AX9FGwsj&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MjI5MjkxNTE1OTYzNDkxMTEzMg%3D%3D.2-ccb7-5&oh=00_AfD_5bwtzXGY3LIGa-67pu0H-Rw1cc30Y8Y7WCyifdV8xg&oe=64447EF6&_nc_sid=1527a3" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-50" style="height: " src="https://scontent-frt3-2.cdninstagram.com/v/t51.2885-15/93810933_272001853828184_5085917630409923189_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_cat=108&_nc_ohc=2Uje8GAJjD0AX9FGwsj&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MjI5MjkxNTE1OTYzNDkxMTEzMg%3D%3D.2-ccb7-5&oh=00_AfD_5bwtzXGY3LIGa-67pu0H-Rw1cc30Y8Y7WCyifdV8xg&oe=64447EF6&_nc_sid=1527a3" alt="">
+        </div>
+    </div>
 </div>
-@endsection class=""
+@endsection 
