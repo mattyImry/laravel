4813eae03b06329cd3667cf768d7e47d7ba702a8
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-18 15:32:37 +0100

    fixed issue with bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="/svg/scatterbrain.svg" alt=""></div>
-                    <div>Imry</div> 
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/scatterbrain.svg" style="height: 50px" alt=""></div>
+                    <div>SoundIn</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
