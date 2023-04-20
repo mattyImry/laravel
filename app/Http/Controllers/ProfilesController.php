2a52270fb9d8f254d270b69f48b1cf8429697efb
Matteo Imeri <matteo.imeri@gmail.com>
2023-04-20 11:00:48 +0100

    fix bug ProfilesController variable

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
 
 class ProfilesController extends Controller
 {
     public function index($user)
     {   
         $user = User::find($user);
-        return view('home');
+        return view('home', ['user'=> $user]);
     }
 }
